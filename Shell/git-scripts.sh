0002a6694e2673c3e523a0457156f6210ed0b16e
fusion809 <brentonhorne77@gmail.com>
2016-11-15 04:00:16 +1000

    Changing paths for slackware-scripts func

--- Shell/git-scripts.sh
+++ Shell/git-scripts.sh
@@ -1,15 +1,15 @@
 ## Update local slackware-scripts repo
 function cps {
   sudo chmod +x -R {$HOME,/root}/Shell/
-  rm -rf $GHUBM/slackware-scripts/Shell && mkdir $GHUBM/slackware-scripts/Shell
-  sudo rm -rf $GHUBM/slackware-scripts/root/Shell
-  cp -a $HOME/Shell/* $GHUBM/slackware-scripts/Shell
-  cp -a $HOME/{.bashrc,.zshrc} $GHUBM/slackware-scripts/
-  sudo cp -a /root/{Shell,.bashrc,.zshrc} $GHUBM/slackware-scripts/root/
+  rm -rf $GHUBM/scripts/slackware-scripts/Shell && mkdir $GHUBM/scripts/slackware-scripts/Shell
+  sudo rm -rf $GHUBM/scripts/slackware-scripts/root/Shell
+  cp -a $HOME/Shell/* $GHUBM/scripts/slackware-scripts/Shell
+  cp -a $HOME/{.bashrc,.zshrc} $GHUBM/scripts/slackware-scripts/
+  sudo cp -a /root/{Shell,.bashrc,.zshrc} $GHUBM/scripts/slackware-scripts/root/
 }
 
 function cdss {
-  cd $GHUBM/slackware-scripts
+  cd $GHUBM/scripts/slackware-scripts
 }
 
 ## Update sabayon-scripts GitHub repo
